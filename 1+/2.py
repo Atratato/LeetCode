ff033bc6fe3ad77035de8857cc55f91cf50547ce
Ralph <Ralphy.t.small@gmail.com>
2024-10-17 15:34:15 +0100

    Finished 2

--- 1+/2.py
+++ 1+/2.py
@@ -1,18 +1,16 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        if l1 == [0]:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1.val == [0]:
             return liToNum(l2)
-        elif l2 == [0]:
+        elif l2.val == [0]:
             return liToNum(l1)
         else:
-            return liToNum(l1) + liToNum(l2)
+            return numToLi(liToNum(l1) + liToNum(l2))
 
         
 def liToNum(l):
@@ -29,21 +27,11 @@ def liToNum(l):
     return num
         
 def numToLi(num):
-    num = str(num)
-    FirstNode = ListNode(num[0])
+    num = str(num)[::-1] #flips the number and turns it into a string
+    FirstNode = ListNode(int(num[0]))
     LastNode = FirstNode
-    for digit in range(len(num)):
-        if digit == 0:
-            continue
-        else:         
-            LastNode.next = ListNode(digit)
-            LastNode = LastNode.next
+    for digit in num[1:]:
+        LastNode.next = ListNode(int(digit))
+        LastNode = LastNode.next
     return FirstNode
-
-three = ListNode(3)
-four = ListNode(4,three)
-two = ListNode(2,four)
-
-print(liToNum(two))
-
-
+    
